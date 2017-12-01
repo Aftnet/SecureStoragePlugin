71e84735341ee554a0bbc759f9e8b90d20e1d089
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-12-01 17:25:45 +0900

    Logic

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alberto Fustinoni")]
+[assembly: AssemblyProduct("Secure Storage Xamarin Plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
